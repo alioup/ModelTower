444d05794aae116c0d0183c91c0a0860ed6c98be
Ali de Jong <ali.dejong@utexas.edu>
2016-04-21 00:59:34 -0500

    actually functional

--- webscraping.py
+++ webscraping.py
@@ -1,8 +1,11 @@
 from lxml import html
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
 import requests
 
-page = requests.get('http://whyisthetowerorange.com/')
-tree = html.fromstring(page.content)
-reason = tree.xpath('//div[@id="reason"]/text()')
+BASE_URL = 'http://whyisthetowerorange.com/'
+html = urlopen(BASE_URL).read()
+soup = BeautifulSoup(html, "lxml")
+reason = soup.select('div[id^=reason]')
 print reason
 
