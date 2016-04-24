47e570a34132c67cfe9dd8f374610107d5e54b3c
Ali de Jong <ali.dejong@utexas.edu>
2016-04-24 11:46:41 -0500

    if else added

--- webscraping.py
+++ webscraping.py
@@ -7,6 +7,8 @@ BASE_URL = 'http://whyisthetowerorange.com/'
 html = urlopen(BASE_URL).read()
 soup = BeautifulSoup(html, "lxml")
 reason = soup.select('div[id^=reason]')
-
-print reason
+if "It's not" in str(reason[0]):
+    print "It's not."
+else:
+    print "The Tower is Orange"
 
