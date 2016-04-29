930fd287a9f610849f4dc78d7572845075f88289
Ali de Jong <ali.dejong@utexas.edu>
2016-04-28 20:50:46 -0500

    time delay added

--- webscraping.py
+++ webscraping.py
@@ -2,13 +2,16 @@ from lxml import html
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 import requests
+import time
 
-BASE_URL = 'http://whyisthetowerorange.com/'
-html = urlopen(BASE_URL).read()
-soup = BeautifulSoup(html, "lxml")
-reason = soup.select('div[id^=reason]')
-if "It's not" in str(reason[0]):
-    print "It's not."
-else:
-    print "The Tower is Orange"
+while 1:
+    BASE_URL = 'http://whyisthetowerorange.com/'
+    html = urlopen(BASE_URL).read()
+    soup = BeautifulSoup(html, "lxml")
+    reason = soup.select('div[id^=reason]')
+    if "It's not" in str(reason[0]):
+        print "It's not."
+    else:
+        print "The Tower is Orange"
+    time.sleep(10)
 
